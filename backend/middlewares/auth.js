03f2325554e4d2ded5a2edf770e2fa12b049a355
evdokim-mellin <evdockimpyatkov.ru@yandex.ru>
2023-02-16 18:58:03 +0300

    Revert "fix: asdasddsa"
    This reverts commit 70e95858747986669a8bd8e9537462bf95497ea5.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,8 +6,6 @@ function auth(req, res, next) {
   const { cookie } = req.headers;
   const { JWT_SECRET = 'dev-key' } = process.env;
 
-  console.log(req.headers);
-
   if (!cookie || !cookie.startsWith('jwt=')) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
