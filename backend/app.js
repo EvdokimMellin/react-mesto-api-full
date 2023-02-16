a563114b062da3c457ff38b686cc07a630fce9e2
evdokim-mellin <evdockimpyatkov.ru@yandex.ru>
2023-02-16 17:53:35 +0300

    fix: разрешить отправку куки

--- backend/app.js
+++ backend/app.js
@@ -34,11 +34,11 @@ app.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
     res.end();
   }
 
